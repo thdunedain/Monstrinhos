32c146aedc455c5f865304152e075cb01acf3818
thdunedain <143745391+thdunedain@users.noreply.github.com>
2023-12-05 14:12:24 -0300

    Update Monstrinhos.py

--- yasmin/Monstrinhos.py
+++ yasmin/Monstrinhos.py
@@ -9,7 +9,7 @@ st.sidebar.header("Opções de Personalização")
 
 # Caixas de seleção para características do monstro
 cor = st.sidebar.selectbox("Escolha a Cor do Monstro", ["Verde", "Azul", "Vermelho","Roxo"])
-forma = st.sidebar.selectbox("Escolha a Forma do Monstro", ["Círculo", "Quadrado", "Triângulo"])
+pele = st.sidebar.selectbox("Escolha a Pele do Monstro", ["Escamas", "Pelos", "Penas","Gosma"])
 habilidades = st.sidebar.multiselect("Escolha as Habilidades do Monstro", ["Voar", "Invisibilidade", "Respirar Fogo","Congelar"])
 
 # Exibindo o monstro personalizado
