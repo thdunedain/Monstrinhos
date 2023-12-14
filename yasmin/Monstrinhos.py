a890009e22077125126d93362d5e3486593a0fe2
thdunedain <143745391+thdunedain@users.noreply.github.com>
2023-12-14 10:31:19 -0300

    Atualizar o Monstrinhos.py

--- yasmin/Monstrinhos.py
+++ yasmin/Monstrinhos.py
@@ -50,4 +50,8 @@ st.write(f"Habilidades: {hability}")
 imagem_combinacao = imagens.get((cor, pele, hability), "url_padrao_da_imagem.png")
 st.image(imagem_combinacao, caption='Monstro Personalizado', use_column_width=True)
 
+João Hallysson Alves da Silva
+Yasmin de Oliveira Martins 
+S2 T-1
+
 
